22003cb5c3210f3feaf7fd8ace81b8490509cdae
Antonin Fischer <tonda.fischer@gmail.com>
2025-08-09 20:44:34 +0200

    Grub entry only.

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -2,9 +2,17 @@
   # Desktop Environment Power Management
   powerManagement.cpuFreqGovernor = "performance";
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.useOSProber = false;
+  boot.loader = {
+    systemd-boot.enable = false;
+    efi.canTouchEfiVariables = false;
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      useOSProber = false;
+      efiSupport = true;
+    };
+  };
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Driver Options
   drivers = {
