75ed5506f9388f3acb3cadf7d577c882ff70fa0d
Antonin Fischer <tonda.fischer@gmail.com>
2025-09-14 14:07:19 +0200

    Starship integration.

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -2,25 +2,34 @@
   pkgs,
   lib,
   ...
-}: {
-  home.packages = with pkgs; [
-    (catppuccin-gtk.override {
-      variant = "mocha";
-      accents = ["mauve"];
-      size = "standard";
-      tweaks = ["rimless"];
-    })
-  ];
+}: let
+  # Build the Catppuccin GTK theme we want
+  catppuccinGtk = pkgs.catppuccin-gtk.override {
+    variant = "mocha";
+    accents = ["mauve"];
+    size = "standard";
+    tweaks = ["rimless"];
+  };
+
+  themeName = "Catppuccin-Mocha-Standard-Mauve-Dark";
+in {
+  # Optional: have the theme in the user profile too (handy for other pickers)
+  home.packages = [catppuccinGtk];
 
   gtk = {
     enable = true;
 
-    # Only select the theme by name; DO NOT set gtk.theme.package here
+    # Select by name only; DO NOT set gtk.theme.package here
     theme = lib.mkForce {
-      name = "Catppuccin-Mocha-Standard-Mauve-Dark";
+      name = themeName;
+      # package = <unset on purpose>;
     };
 
     gtk3.extraConfig = {gtk-application-prefer-dark-theme = 1;};
     gtk4.extraConfig = {gtk-application-prefer-dark-theme = 1;};
   };
+
+  # Manually provide the source for ~/.themes/<name> so HM doesn't produce null
+  home.file.".themes/${themeName}".source =
+    lib.mkForce "${catppuccinGtk}/share/themes/${themeName}";
 }
