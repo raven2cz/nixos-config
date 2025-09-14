8d6da1deb9c072105591f6e266d73de197cd73d1
Antonin Fischer <tonda.fischer@gmail.com>
2025-09-14 13:57:13 +0200

    Starship integration.

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -1,35 +1,39 @@
-{ lib, inputs, ... }: 
 {
+  lib,
+  inputs,
+  ...
+}: {
   programs.starship = {
     enable = true;
-
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableNushellIntegration = true;
 
-    settings = {
-      # right_format = "$cmd_duration";
-      
-      directory = {
-        format = "[ ](bold #89b4fa)[ $path ]($style)";
-        style = "bold #b4befe";
-      };
+    settings = let
+      catpp_mocha =
+        builtins.fromTOML
+        (builtins.readFile "${inputs."catppuccin-starship"}/themes/mocha.toml");
+    in
+      {
+        directory = {
+          format = "[ ](bold #89b4fa)[ $path ]($style)";
+          style = "bold #b4befe";
+        };
+        character = {
+          success_symbol = "[ ](bold #89b4fa)[ ➜](bold green)";
+          error_symbol = "[ ](bold #89b4fa)[ ➜](bold red)";
+        };
+        cmd_duration = {
+          format = "[󰔛 $duration]($style)";
+          disabled = false;
+          style = "bg:none fg:#f9e2af";
+          show_notifications = false;
+          min_time_to_notify = 60000;
+        };
 
-      character = {
-        success_symbol = "[ ](bold #89b4fa)[ ➜](bold green)";
-        error_symbol = "[ ](bold #89b4fa)[ ➜](bold red)";
-        # error_symbol = "[ ](bold #89dceb)[ ✗](bold red)";
-      };
-
-      cmd_duration = {
-        format = "[󰔛 $duration]($style)";
-        disabled = false;
-        style = "bg:none fg:#f9e2af";
-        show_notifications = false;
-        min_time_to_notify = 60000;
-      };        
-
-      palette = "catppuccin_mocha";
-    } // builtins.fromTOML (builtins.readFile "${inputs.catppuccin-starship}/palettes/mocha.toml");
+        palette = "catppuccin_mocha";
+      }
+      # combine pallete tables Catppuccin (includes [palettes.catppuccin_mocha])
+      // catpp_mocha;
   };
 }
