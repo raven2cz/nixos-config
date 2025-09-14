81f009687a72139cd8df540f4a199cda5744fe93
Antonin Fischer <tonda.fischer@gmail.com>
2025-09-14 12:18:46 +0200

    Use stylix icons for gtk

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -31,11 +31,6 @@
       };
     };
 
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-
     gtk3.extraConfig = {gtk-application-prefer-dark-theme = 1;};
     gtk4.extraConfig = {gtk-application-prefer-dark-theme = 1;};
   };
